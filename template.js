28f8dbcbee9ba4625249ed873eb1f49c3cf0a2eb
Hamit Mizrak <hamitmizrak@gmail.com>
2023-12-26 18:08:32 +0300

    console,alert

--- template.js
+++ template.js
@@ -1,1 +1,30 @@
-console.log("external js")
+//console.log("external js")
+
+// Single Comment
+/*
+multi Comment
+multi Comment
+multi Comment
+*/
+
+////////////////////////////////////////////////////
+// window.
+// document
+console.log("Merhabalar")
+
+// Log
+window.console.log("Window Console Log");
+
+// Warn
+console.warn("console.warn");
+
+// Info
+console.info("console.info");
+
+// Error
+console.error("console.error");
+
+// alert("Alert")
+// window. alert("Alert");
+
+////////////////////////////////////////////////////
