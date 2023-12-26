aeb222edf942348a2c66bf6362c287ccc81e9b0b
Hamit Mizrak <hamitmizrak@gmail.com>
2023-12-26 18:27:03 +0300

    variables

--- template.js
+++ template.js
@@ -10,21 +10,64 @@ multi Comment
 ////////////////////////////////////////////////////
 // window.
 // document
-console.log("Merhabalar")
+// console.log("Merhabalar")
 
-// Log
-window.console.log("Window Console Log");
+// // Log
+// window.console.log("Window Console Log");
 
-// Warn
-console.warn("console.warn");
+// // Warn
+// console.warn("console.warn");
 
-// Info
-console.info("console.info");
+// // Info
+// console.info("console.info");
 
-// Error
-console.error("console.error");
+// // Error
+// console.error("console.error");
 
-// alert("Alert")
-// window. alert("Alert");
+// // alert("Alert")
+// // window. alert("Alert");
 
 ////////////////////////////////////////////////////
+
+// Variable
+// var, let, const
+// Common Rules
+var companyName="Miuul";
+console.log(companyName);
+
+var companyName="Miuul";
+console.log(companyName);
+
+var companyName44="Miuul";
+console.log(companyName44);
+
+var _companyName44="Miuul";
+console.log(_companyName44);
+
+var $_companyName44="Miuul";
+console.log($_companyName44);
+
+// yazılmaz
+// var üğşçö~30$_companyName44="Miuul";
+// console.log($_companyName44);
+
+// LET
+let companyName2="Miuul Frontend";
+companyName2="Miuul Frontend Data";
+console.warn(companyName2);
+
+// Kullanamazsınız.
+// let companyName2="Miuul Frontend";
+// console.warn(companyName2);
+
+// CONST
+const companyName3="Miuul Frontend JS";
+console.error(companyName3);
+
+// Kullanamazsınız.
+// companyName3="Miuul Frontend JS HTML5";
+// console.error(companyName3);
+
+// Kullanamazsınız.
+// const companyName3="Miuul Frontend JS";
+// console.error(companyName3);
